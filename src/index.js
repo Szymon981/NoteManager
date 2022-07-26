320a6c86b122115eeeaf27f2221b748d1f33d9a3
Szymon Chytaj <s.chytaj@webcon.com>
2022-07-26 14:19:26 +0200

    introduction-to-redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from "@reduxjs/toolkit";
+import rootReducer from "./store/reducer";
+// import reducerA from "./store/reducerA";
+// import reducerB from "./store/reducerB";
+
+// const rootReducer = combineReducers({
+//   rA: reducerA,
+//   rB: reducerB,
+// });
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
